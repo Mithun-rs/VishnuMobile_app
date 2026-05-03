4ec845fa8e820fef5c629ec0151cb42c4c30bed4
Mithun RS <mithunsivanantham183@gmail.com>
2026-05-03 12:26:51 +0530

    fix: add confirm=t to updated  APK download URL

--- android/app/src/main/java/com/vishnumobile/MainApplication.kt
+++ android/app/src/main/java/com/vishnumobile/MainApplication.kt
@@ -10,6 +10,8 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.lugg.RNCConfig.RNCConfigPackage
+import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -17,8 +19,9 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+              add(RNCConfigPackage())
+              add(RNDateTimePickerPackage())
+             
             }
 
         override fun getJSMainModuleName(): String = "index"
@@ -33,10 +36,9 @@ class MainApplication : Application(), ReactApplication {
     get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
-    super.onCreate()
+    super.onCreate()  // ✅ NO SplashScreen here
     SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
   }
